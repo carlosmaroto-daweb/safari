9568727abf9fd837c368c40fca5a7953902d99c9
Carlos Maroto <carlosmaroto.daweb@gmail.com>
2023-06-05 16:31:47 +0200

    v1.0

--- js/conservation.js
+++ js/conservation.js
@@ -1,5 +1,4 @@
-var finish = false;
-var display = false;
+var counters_finish = [];
 
 function isInViewport(elem) {
     let distance = elem.getBoundingClientRect();
@@ -9,33 +8,28 @@ function isInViewport(elem) {
 }
 
 function incrementCounter(counter) {
-    if (!finish) {
-        counter.innerText = "0";
-        const updateCounter = () => {
-            const target = +counter.getAttribute("data-target");
-            const c = +counter.innerText;
-            const increment = target*1.0/100;
-            if (c < target) {
-                counter.innerText = `${Math.ceil(c + increment)}`;
-                setTimeout(updateCounter, 5);
-            } else {
-                counter.innerText = target;
-            }
-        };
-        updateCounter();
-    }
+    counter.innerText = "0";
+    const updateCounter = () => {
+        const target = +counter.getAttribute("data-target");
+        const c = +counter.innerText;
+        const increment = target*1.0/100;
+        if (c < target) {
+            counter.innerText = `${Math.ceil(c + increment)}`;
+            setTimeout(updateCounter, 5);
+        } else {
+            counter.innerText = target;
+        }
+    };
+    updateCounter();
 };
 
 function manageAnimations() {
     let elements = document.getElementsByClassName('counter');
     for (let i=0; i<elements.length; i++) {
-        if (isInViewport(elements[i])) {
+        if (isInViewport(elements[i]) && !counters_finish.includes(elements[i].getAttribute("id"))) {
             incrementCounter(elements[i]);
-            display = true;
+            counters_finish.push(elements[i].getAttribute("id"));
         }
-    }
-    if (display) {
-        finish = true;
     }
 }
 
