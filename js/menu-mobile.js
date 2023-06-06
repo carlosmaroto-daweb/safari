bed48344c2b02553d81a4ff235971a7cdb1ae1b7
Carlos Maroto <carlosmaroto.daweb@gmail.com>
2023-06-06 22:12:44 +0200

    v1.0

--- js/menu-mobile.js
+++ js/menu-mobile.js
@@ -1,3 +1,5 @@
+/********************************** MENU MOBILE **********************************/
+
 const header = document.getElementById('header');
 const menu_collapse = document.getElementById('menu-collapse');
 const menu_mobile = document.getElementById('menu-mobile');
@@ -13,3 +15,31 @@ menu_collapse.addEventListener("click", function(){
         menu_collapse.classList.remove('menu-collapse-active');
     }
 });
+
+/********************************** SCROLL TO TOP **********************************/
+
+let scroll_to_top = document.getElementById('scroll-to-top');
+
+function isInBottomHalf() {
+    let result;
+    let middle = document.body.clientHeight/2;
+    if(window.scrollY>middle) {
+        result = true;
+    }
+    else {
+        result = false;
+    }
+    return result;
+}
+
+function manageAnimations() {
+    if (isInBottomHalf()) {
+        scroll_to_top.classList.replace('animate-slide-top', 'animate-slide-top-viewport');
+    }
+    else {
+        scroll_to_top.classList.replace('animate-slide-top-viewport', 'animate-slide-top');
+    }
+}
+
+window.addEventListener('load', manageAnimations);
+window.addEventListener('scroll', manageAnimations);
